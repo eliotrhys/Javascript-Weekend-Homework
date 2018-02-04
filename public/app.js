0fae545a636bb68c238a18c16b40d6c5befca11a
eliotrhys <eliot@eliotshort.co.uk>
2018-02-04 17:50:31 +0000

    safety bounce

--- public/app.js
+++ public/app.js
@@ -6,32 +6,12 @@ var makeRequest = function(url, callback){
 }
 
 var requestComplete = function(){
-    if (this.status !== 200) return;
-    var jsonString = this.responseText;
-    var BKVInfo = JSON.parse(jsonString);
-    fuckArray = BKVInfo.data.results;
-    // console.log(fuckArray);
-    // populateList(fuckArray);
-    populateGrid(fuckArray);
-
-  }
-
-// var populateList = function(BKV_comics){
-//   var ul = document.querySelector("#testlist");
-//
-//   for (comic of BKV_comics){
-//     var li = document.createElement("li");
-//     // li.innerText = comic.title;
-//
-//     var img = document.createElement("img");
-//     var imgBuilder = comic.thumbnail.path;
-//     var imgBuilder2 = comic.thumbnail.extension;
-//     var imgBuilt = `${imgBuilder}/portrait_medium.${imgBuilder2}`;
-//     img.src = imgBuilt;
-//     ul.appendChild(li);
-//     ul.appendChild(img);
-//   }
-// }
+  if (this.status !== 200) return;
+  var jsonString = this.responseText;
+  var BKVInfo = JSON.parse(jsonString);
+  fuckArray = BKVInfo.data.results;
+  populateGrid(fuckArray);
+}
 
 var populateGrid = function(BKV_comics){
   var div = document.querySelector("#the_shop");
@@ -39,24 +19,33 @@ var populateGrid = function(BKV_comics){
   for (comic of BKV_comics){
     var subDiv = document.createElement("div");
     subDiv.setAttribute("class", "border");
+    subDiv.setAttribute("style", "hover: ");
+    // subDiv.setAttribute("onclick", "setDescription(comic.description)");
 
     var img = document.createElement("img");
     var imgBuilder = comic.thumbnail.path;
     var imgBuilder2 = comic.thumbnail.extension;
-    var imgBuilt = `${imgBuilder}/portrait_medium.${imgBuilder2}`;
+    var imgBuilt = `${imgBuilder}/portrait_xlarge.${imgBuilder2}`;
     img.src = imgBuilt;
-    console.log(subDiv);
-    console.log(div);
+    img.setAttribute("style", "")
+
+    var button = document.createElement("button");
+    button.innerText = "BUY NOW!";
 
     subDiv.appendChild(img);
+    subDiv.appendChild(button);
     div.appendChild(subDiv);
   }
 }
 
+// var setDescription = function(description){
+//   var div = document.querySelector("#the_description");
+//   div.innerText = description;
+// }
 
 var app = function(){
-    var url = 'https://gateway.marvel.com:443/v1/public/creators/190/comics?ts=1&limit=25&apikey=6c92e56a5d9dc2a50364be82b90792dd&hash=516fd98028f4197461503a883d78786d';
-    makeRequest(url, requestComplete);
-  }
+  var url = 'https://gateway.marvel.com:443/v1/public/creators/190/comics?ts=1&limit=15&apikey=6c92e56a5d9dc2a50364be82b90792dd&hash=516fd98028f4197461503a883d78786d';
+  makeRequest(url, requestComplete);
+}
 
 window.addEventListener('load', app);
